c18413cf09b9ac4bfde49587f2b01c58093c4144
juliacribeiro <juliacrmg@gmail.com>
2025-11-11 20:11:04 -0300

    v1.2.0

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import { Principal } from "./pages/Principal";
 import { Produto } from "./pages/Produto";
 import { Carrinho } from "./pages/Carrinho"
 import {carrosseis,produto,filtro} from "./data.json"
-import VLibras from "@djpfs/react-vlibras";
-
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 
 const App = () => {
@@ -15,7 +13,6 @@ const App = () => {
                 <Route path="/produto/:id" element={<Produto/>}/>
                 <Route path="/carrinho" element={<Carrinho produto={produto}/>}/>
             </Routes>
-            <VLibras forceOnload={true}/>
         </Router>
     );
 }
